6afb36ba327b802b91013e9dcd687e5e96af2bbf
skeown13 <code@keown.net>
2020-05-05 14:25:08 -0700

    creating some variables for generating random numbers

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  let optionsRndNum = Math.floor((Math.random() * 12) + 1);
+  let targetRndNum = Math.floor((Math.random() * 111) + 19);
+  
+  let balloonRndNum = optionsRndNum;
+  let burnerRndNum = optionsRndNum;
+  let ropeRndNum = optionsRndNum;
+  let xtargetRndNum = optionsRndNum;
+
+  let currentScore;
+  let targetScore = targetRndNum;
+
+})
