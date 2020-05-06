2db56d6174d1305b229d96ec2ce23b7b7efe010e
skeown13 <code@keown.net>
2020-05-06 13:18:33 -0700

    comment cleanup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  // let optionsRndNum = Math.floor((Math.random() * 12) + 1);
+
+  // Create functions that generate random numbers for the options clicks as well as the Target Number
   function optionsRndNum() {
     return Math.floor((Math.random() * 12) + 1);
   }
@@ -8,7 +9,7 @@ $(document).ready(function() {
     return Math.floor((Math.random() * 111) + 19); 
   } 
   
-  
+  // Declare variables to hold information
   let balloonRndNum;
   let burnerRndNum;
   let ropeRndNum;
@@ -19,6 +20,7 @@ $(document).ready(function() {
   let wins = 0;
   let losses = 0;
   
+  // Create a reset function that assigns random numbers to each of the options and to the Target Score. Also sets the Current Score back to 0 at the end of each round. And shows the new Target Score on the screen.
   function reset() {
     optionsRndNum();
     balloonRndNum = optionsRndNum();
@@ -32,10 +34,11 @@ $(document).ready(function() {
     $("#actualTargetScore").text(targetScore);
   }
 
+  // Begins the game by assigning necessary variables via the reset function.
   reset();
-  // $("#actualTargetScore").text(targetScore);
-  console.log(targetScore);
-  // Needed feature for later - if a number has already randomly been assigned to an option, do not allow that number to be assigned to another option
+  
+  // When an option is clicked, it ads its assigned random number to the Current Score.
+  // ***Needed feature for later - if a number has already randomly been assigned to an option, do not allow that number to be assigned to another option***
   $(".option").on("click", function() {
     if (this.id === "balloon") {
       currentScore += balloonRndNum;
@@ -51,6 +54,7 @@ $(document).ready(function() {
       $("#actualCurrentScore").text(currentScore);
     }
 
+    // Creates the Logic for when a person wins or losses and what to do at that point.
     if (currentScore === targetScore) {
       wins += 1;
       $("#winTotal").text(wins);
