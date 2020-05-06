ac361da0d3626a036d7336d44de6e6ef1cb0fd60
skeown13 <code@keown.net>
2020-05-06 12:34:14 -0700

    put targetRndNum into a function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
     return Math.floor((Math.random() * 12) + 1);
   }
   
-  let targetRndNum = Math.floor((Math.random() * 111) + 19);
+  function targetRndNum() {
+    Math.floor((Math.random() * 111) + 19); 
+  } 
 
   let balloonRndNum = optionsRndNum();
   let burnerRndNum = optionsRndNum();
